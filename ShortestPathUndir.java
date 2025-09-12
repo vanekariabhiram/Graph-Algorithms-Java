89ae606f2cb862a8ecc4b9f7819d1814084b06e9
vanekariabhiram <vannekariabhiram@gmail.com>
2025-09-12 23:41:36 +0530

    Update ShortestPathUndir.java

--- ShortestPathUndir.java
+++ ShortestPathUndir.java
@@ -27,3 +27,26 @@ public class ShortestPathUndir {
 return distance;
     }
 }
+    // method2:when destination is not given and without using visited array
+      static int[] bfs2(int src ,List<List<Integer>>adj) {
+        int n=adj.size();
+
+        int[] distance = new int[n];
+        Arrays.fill(distance, -1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+
+        distance[src] = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (int neighbour : adj.get(node)) {
+                if (distance[neighbour]==-1) {
+                    distance[neighbour] = distance[node] + 1;
+                    queue.offer(neighbour);
+                }
+
+            }
+        }
+        return distance;
+    }
